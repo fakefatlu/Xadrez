5ea7e525b66c4ad701382a7df5b39457061ff6c2
fakefatlu <luana199835@hotmail.com>
2020-04-13 14:50:08 -0300

    Implementação do projeto

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,10 +1,14 @@
 package Aplicacao;
 
+import jogoTabuleiro.Tabuleiro;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		
+		
 	}
 
 }
--- src/jogoTabuleiro/Tabuleiro.java
+++ src/jogoTabuleiro/Tabuleiro.java
@@ -1,5 +1,35 @@
 package jogoTabuleiro;
 
 public class Tabuleiro {
+	private int quant_linhas;
+	private int quant_coluna;
+	private Peca[][] pecas;
+	
+	
+	public Tabuleiro(int quant_linhas, int quant_coluna) {
+		this.quant_linhas = quant_linhas;
+		this.quant_coluna = quant_coluna;
+		
+		pecas = new Peca[quant_linhas][quant_coluna];
+	}
 
+
+	public int getQuant_linhas() {
+		return quant_linhas;
+	}
+
+
+	public void setQuant_linhas(int quant_linhas) {
+		this.quant_linhas = quant_linhas;
+	}
+
+
+	public int getQuant_coluna() {
+		return quant_coluna;
+	}
+
+
+	public void setQuant_coluna(int quant_coluna) {
+		this.quant_coluna = quant_coluna;
+	}
 }
